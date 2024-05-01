0e46928b593a286ec1b540dad70668a0444a9746
Katelyn Mak <k.mak@student.unsw.edu.au>
2024-05-01 16:23:28 +1000

    renamed ports and assigned registers instead of ports directly to synchronous logic

--- hdl/src/dtw_ref.v
+++ hdl/src/dtw_ref.v
@@ -12,25 +12,25 @@ module dtw_ref #(
     input   wire                    rst,
     input   wire                    rs,
 
-    input   wire [REFMEM_PTR_WIDTH-1 : 0] ref_len,
-    input   wire                    op_mode,            // Reference mode: 0, query mode: 1
-    output  reg                     busy,               // Idle: 0, busy: 1
+    input   wire [REFMEM_PTR_WIDTH-1 : 0]       ref_len_in,
+    input   wire                                op_mode_in,            // Reference mode: 0, query mode: 1
+    output  wire                                 busy_out,               // Idle: 0, busy_out: 1
 
     // dtw core interfacing signals
-    input  wire                                 dtw_done,
-    input  wire [REFMEM_PTR_WIDTH-1:0]          dtw_read_addr,
+    input  wire                                 dtw_done_in,
+    input  wire [REFMEM_PTR_WIDTH-1:0]          dtw_read_addr_in,
     output wire [WIDTH-1:0]                     ref_data_out,
-    output wire                                 load_done,
+    output wire                                 load_done_out,
 
     // Src FIFO signals
-    output  wire                    src_fifo_clear_out,     // Src FIFO Clear signal
-    output  reg                     src_fifo_rden_out,      // Src FIFO Read enable
-    input   wire                    src_fifo_empty,     // Src FIFO Empty
-    input   wire [WIDTH-1:0]             src_fifo_data_in,      // Src FIFO Data
+    output  wire                                src_fifo_clear_out,     // Src FIFO Clear signal
+    output  wire                                src_fifo_rden_out,      // Src FIFO Read enable
+    input   wire                                src_fifo_empty,     // Src FIFO Empty
+    input   wire [WIDTH-1:0]                    src_fifo_data_in,      // Src FIFO Data
 
     // debug signals
-    output  wire [1:0]                              dbg_ref_state,
-    output  wire [REFMEM_PTR_WIDTH-1:0]             dbg_addr_ref
+    output  wire [1:0]                          dbg_ref_state_out,
+    output  wire [REFMEM_PTR_WIDTH-1:0]         dbg_addr_ref_out
 );
 
 /* ===============================
@@ -50,15 +50,19 @@ localparam [1:0]
  * registers/wires
  * =============================== */
 // state
-reg [1:0] ref_state;
+reg [1:0]                       ref_state;
 
 // module registers
-reg [REFMEM_PTR_WIDTH-1:0] ref_addr_reg;
-reg dtw_read_addr_mode_reg;
+reg [REFMEM_PTR_WIDTH-1:0]      ref_addr_reg;
+reg                             dtw_read_addr_in_mode_reg;
+reg                             load_done_reg;
+reg                             src_fifo_rden_reg;
+reg                             src_fifo_clear_reg;
+reg                             busy_reg;
 
 // ref mem nodes
-wire [REFMEM_PTR_WIDTH-1:0] addr_node;
-reg                 ref_wren_node;           // Write enable for refmem
+wire [REFMEM_PTR_WIDTH-1:0]     addr_node;
+reg                             ref_wren_node;           // Write enable for refmem
 
 /* ===============================
  * submodules
@@ -80,11 +84,15 @@ dtw_core_ref_mem #(
 /* ===============================
  * asynchronous logic
  * =============================== */
-assign addr_node = (dtw_read_addr_mode_reg) ? dtw_read_addr : ref_addr_reg;
+assign addr_node = (dtw_read_addr_in_mode_reg) ? dtw_read_addr_in : ref_addr_reg;
+assign load_done_out = load_done_reg;
+assign src_fifo_rden_out = src_fifo_rden_reg;
+assign src_fifo_clear_out = src_fifo_clear_reg;
+assign busy_out = busy_reg;
 
 // debug
-assign dbg_ref_state = ref_state;
-assign dbg_addr_ref = addr_node;
+assign dbg_ref_state_out = ref_state;
+assign dbg_addr_ref_out = addr_node;
 
 
 /* ===============================
@@ -98,9 +106,9 @@ always @(posedge clk) begin
         case (ref_state)
         REF_IDLE: begin
             if (rs) begin
-                if (op_mode == MODE_NORMAL && load_done == 1) begin
+                if (op_mode_in == MODE_NORMAL && load_done_reg == 1) begin
                     ref_state <= REF_DTW_READ;
-                end else if (op_mode == MODE_LOAD_REF && load_done == 0) begin
+                end else if (op_mode_in == MODE_LOAD_REF && load_done_reg == 0) begin
                     ref_state <= REF_LOAD;
                 end else begin
                     ref_state <= REF_IDLE;
@@ -111,7 +119,7 @@ always @(posedge clk) begin
         end
 
         REF_LOAD: begin
-            if (ref_addr_reg[REFMEM_PTR_WIDTH-1:0] < ref_len[REFMEM_PTR_WIDTH-1:0]) begin
+            if (ref_addr_reg[REFMEM_PTR_WIDTH-1:0] < ref_len_in[REFMEM_PTR_WIDTH-1:0]) begin
                 ref_state <= REF_LOAD;
             end else begin
                 ref_state <= REF_IDLE;
@@ -119,7 +127,7 @@ always @(posedge clk) begin
         end
 
         REF_DTW_READ: begin
-            if (dtw_done) begin
+            if (dtw_done_in) begin
                 ref_state <= REF_IDLE;
             end else begin
                 ref_state <= REF_DTW_READ;
@@ -137,20 +145,20 @@ end
 always @(posedge clk) begin
     case (ref_state)
     REF_IDLE: begin
-        src_fifo_rden_out   <= 0;
-        ref_addr_reg       <= 0;
-        ref_wren_node            <= 0;
-        src_fifo_clear_out  <= 1;
-        busy <= 0;
+        src_fifo_rden_reg   <= 0;
+        ref_addr_reg        <= 0;
+        ref_wren_node       <= 0;
+        src_fifo_clear_reg  <= 1;
+        busy_reg <= 0;
     end
 
     REF_LOAD: begin
-        src_fifo_rden_out   <= 1;
-        src_fifo_clear_out  <= 0;
-        busy <= 1;
+        src_fifo_rden_reg   <= 1;
+        src_fifo_clear_reg  <= 0;
+        busy_reg <= 1;
 
         // When the output data from the fifo is valid
-        if (!src_fifo_empty && src_fifo_rden_out) begin
+        if (!src_fifo_empty && src_fifo_rden_reg) begin
             ref_addr_reg   <= ref_addr_reg + 1; // next address to read from
             ref_wren_node        <= 1;
         end else begin
@@ -159,20 +167,20 @@ always @(posedge clk) begin
     end
 
     REF_DTW_READ: begin
-        src_fifo_rden_out   <= 0;
+        src_fifo_rden_reg   <= 0;
         ref_wren_node            <= 0;
-        src_fifo_clear_out  <= 0;
+        src_fifo_clear_reg  <= 0;
         ref_addr_reg       <= 0;
-        busy <= 1;
+        busy_reg <= 1;
 
     end
 
     default: begin
-        src_fifo_rden_out   <= 0;
+        src_fifo_rden_reg   <= 0;
         ref_addr_reg       <= 0;
         ref_wren_node            <= 0;
-        src_fifo_clear_out  <= 0;
-        busy <= 0;
+        src_fifo_clear_reg  <= 0;
+        busy_reg <= 0;
     end
     endcase
 end
@@ -180,19 +188,19 @@ end
 // Ref Load FSM Output
 always @(posedge clk) begin
     if (rst) begin
-        load_done <= 1'b0;
+        load_done_reg <= 1'b0;
     end else begin
         case(ref_state)
             REF_LOAD: begin
-                if (ref_addr_reg[REFMEM_PTR_WIDTH-1:0] < ref_len[REFMEM_PTR_WIDTH-1:0]) begin
-                    load_done <= 1'b0;
+                if (ref_addr_reg[REFMEM_PTR_WIDTH-1:0] < ref_len_in[REFMEM_PTR_WIDTH-1:0]) begin
+                    load_done_reg <= 1'b0;
                 end else begin
-                    load_done <= 1'b1;
+                    load_done_reg <= 1'b1;
                 end
             end
 
             default: begin
-                load_done <= load_done;
+                load_done_reg <= load_done_reg;
             end
         endcase
     end
@@ -201,31 +209,31 @@ end
 // addr mode FSM output
 always @(posedge clk) begin
     if (rst) begin
-        dtw_read_addr_mode_reg  <= 1;
+        dtw_read_addr_in_mode_reg  <= 1;
     end else begin
         case(ref_state)
             REF_IDLE: begin
-                if (op_mode == MODE_NORMAL && load_done == 1) begin
-                    dtw_read_addr_mode_reg <= 1'b1;
+                if (op_mode_in == MODE_NORMAL && load_done_reg == 1) begin
+                    dtw_read_addr_in_mode_reg <= 1'b1;
                 end else begin
-                    dtw_read_addr_mode_reg <= 1'b0;
+                    dtw_read_addr_in_mode_reg <= 1'b0;
                 end
             end
 
             REF_LOAD: begin
-                if (ref_addr_reg[REFMEM_PTR_WIDTH-1:0] < ref_len[REFMEM_PTR_WIDTH-1:0]) begin
-                    dtw_read_addr_mode_reg <= 1'b0;
+                if (ref_addr_reg[REFMEM_PTR_WIDTH-1:0] < ref_len_in[REFMEM_PTR_WIDTH-1:0]) begin
+                    dtw_read_addr_in_mode_reg <= 1'b0;
                 end else begin
-                    dtw_read_addr_mode_reg <= 1'b1;
+                    dtw_read_addr_in_mode_reg <= 1'b1;
                 end
             end
 
             REF_DTW_READ: begin
-                dtw_read_addr_mode_reg <= 1'b1;
+                dtw_read_addr_in_mode_reg <= 1'b1;
             end
 
             default: begin
-                dtw_read_addr_mode_reg <= 1'b0;
+                dtw_read_addr_in_mode_reg <= 1'b0;
             end
         endcase
     end
